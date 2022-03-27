9ff67af4176dc34a9b78a0d6b568c2aaf4ee9c4c
eduardor2m <deveduardomelo@gmail.com>
2022-03-27 08:27:29 -0300

    feat: user features

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { AppError } from "../errors/AppError";
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError("JWT token is missing", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub } = verify(
+      token,
+      "854a5673d6901228e289b5a63b539f4a"
+    ) as IPayload;
+    const usersRepository = new UsersRepository();
+
+    const user = await usersRepository.findById(sub);
+
+    if (!user) {
+      throw new AppError("User does not exists", 401);
+    }
+
+    next();
+  } catch (err) {
+    throw new AppError("Invalid JWT token", 401);
+  }
+}
---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,58 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { AppError } from "../../../../errors/AppError";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError("Email or password incorrect");
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError("Email or password incorrect");
+    }
+
+    const token = sign({}, "854a5673d6901228e289b5a63b539f4a", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
